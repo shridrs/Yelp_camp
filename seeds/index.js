02b8a17370a8366f235f84231e543db185bf804d
Divyashri R S <shridrs14@gmail.com>
2023-08-03 21:21:57 +0530

    added Review Module

---
+++ seeds/index.js
@@ -0,0 +1,73 @@
+
+
+
+const mongoose = require('mongoose');
+const cities = require('./cities');
+const { places, descriptors } = require('./seedHelpers');
+const Campground = require('../models/campground');
+
+mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
+
+
+const db=mongoose.connection;
+db.on('error',console.error.bind(console,"connection error:("));
+db.once('open',()=>{
+    console.log('database connected');
+});
+const sample = array => array[Math.floor(Math.random() * array.length)];
+
+
+const seedDB=async ()=>{
+    await Campground.deleteMany({});
+    for(let i=0;i<50;i++){
+        const random1000=Math.floor(Math.random()*1000);
+        const price=Math.floor(Math.random()*20)+1;
+
+        const camp =new Campground({
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            title:`${sample(descriptors)} ${sample(places)} `,
+            image:`https://source.unsplash.com/collection/483251/`,
+            description:'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea cupiditate, magni, harum beatae fugiat nobis dolorum porro doloreblanditiis nemo suscipit ',
+            price
+        })
+    await camp.save();
+
+    }
+}
+
+seedDB().then(()=>{
+    mongoose.connection.close();
+});
+
+
+
+
+
+
+
+
+// const db = mongoose.connection;
+
+// db.on("error", console.error.bind(console, "connection error:"));
+// db.once("open", () => {
+//     console.log("Database connected");
+// });
+
+// const sample = array => array[Math.floor(Math.random() * array.length)];
+
+
+// const seedDB = async () => {
+//     await Campground.deleteMany({});
+//     for (let i = 0; i < 50; i++) {
+//         const random1000 = Math.floor(Math.random() * 1000);
+//         const camp = new Campground({
+//             location: `${cities[random1000].city}, ${cities[random1000].state}`,
+//             title: `${sample(descriptors)} ${sample(places)}`
+//         })
+//         await camp.save();
+//     }
+// }
+
+// seedDB().then(() => {
+//     mongoose.connection.close();
+// })
